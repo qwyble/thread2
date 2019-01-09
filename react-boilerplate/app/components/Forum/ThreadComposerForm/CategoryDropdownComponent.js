2eb206ff28e76299cbaad54af711dca5ed5c159d
Quinten Wyble <quinten.wyble@allegiantair.com>
2019-01-09 10:53:26 -0800

    implement react-boilerplate

--- react-boilerplate/app/components/Forum/ThreadComposerForm/CategoryDropdownComponent.js
+++ react-boilerplate/app/components/Forum/ThreadComposerForm/CategoryDropdownComponent.js
@@ -1,89 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import axios from 'axios';
-import {Dropdown} from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
-
-
-class CategoryDropdownComponent extends React.Component{
-  static propTypes = {
-    placeholder: PropTypes.string,
-    value: PropTypes.number,
-    name: PropTypes.string,
-    validate: PropTypes.func,
-    onChange: PropTypes.func
-  }
-
-  state = {
-    value: this.props.value,
-    searchValue: '',
-    cats: [],
-    error: ''
-  }
-
-  componentDidMount(){
-    this.getCategories('%');
-  }
-
-
-  getCategories = (searchString) => {
-    axios({
-      method: 'get',
-      url: 'https://thread-204819.appspot.com/getForumCategories',
-      params: {
-        searchString: searchString
-      }
-    }).then(result => {
-      var cats = [];
-      cats = result.data.map((cat, i) => {
-        return {
-          text: cat.category,
-          key: cat.idthreadcategories,
-          value: cat.idthreadcategories
-        }
-      })
-      this.setState({cats});
-    })
-  }
-
-
-  onSearchChange = (e, d) => {
-    const searchValue = e.target.value || '%';
-    const error = this.props.validate ? this.props.validate(searchValue) : false;
-
-    this.setState({ error, searchValue }, () => {
-      this.getCategories(this.state.searchValue);
-    });
-
-  }
-
-
+class CategoryDropdownComponent extends React.Component {
   onDropdownChange = (e, d) => {
+    this.props.onChange(d);
+  };
 
-    const name = d.name;
-    const value = d.value;
-    const error = this.props.validate(value);
-
-    this.setState({error, value})
-
-    this.props.onChange({name, value, error});
-  }
-
-
-
-  render(){
-    return(
+  render() {
+    return (
       <Dropdown
-        fluid search selection
+        fluid
+        search
+        selection
         name={this.props.name}
-        placeholder={this.props.placeholder}
-        options={this.state.cats}
-        value={this.state.value}
+        value={this.props.value}
+        options={this.props.categories}
         onChange={this.onDropdownChange}
-        onSearchChange={this.onSearchChange}
+        placeholder={this.props.placeholder}
       />
-    )
+    );
   }
 }
+
+CategoryDropdownComponent.propTypes = {
+  name: PropTypes.string,
+  value: PropTypes.string,
+  categories: PropTypes.array,
+  onChange: PropTypes.func,
+  placeholder: PropTypes.string,
+};
 
 export default CategoryDropdownComponent;
--- react-boilerplate/app/components/Forum/ThreadComposerForm/index.js
+++ react-boilerplate/app/components/Forum/ThreadComposerForm/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Container, Form, Button, Input } from 'semantic-ui-react';
+import LoaderWrapper from 'containers/Wrappers/LoaderWrapper';
 import CategoryDropdownComponent from './CategoryDropdownComponent';
 
 class ThreadComposer extends React.Component {
@@ -36,41 +37,50 @@ class ThreadComposer extends React.Component {
       field => field.error.length
     );
 
+    const CategoryDropdown = (
+      <Form.Field required error={!!this.state.category.error}>
+        <CategoryDropdownComponent
+          name="category"
+          placeholder="Choose thread category"
+          value={this.state.category.value}
+          onChange={this.handleInputChange}
+          categories={this.props.categories}
+        />
+        {this.state.category.error}
+      </Form.Field>
+    );
+
+    const SubjectComponent = (
+      <Form.Field required error={!!this.state.subject.error}>
+        <Input
+          name="subject"
+          placeholder="Subject"
+          value={this.state.subject.value}
+          onChange={this.handleInputChange}
+        />
+        {this.state.subject.error}
+      </Form.Field>
+    );
+
+    const BodyComponent = (
+      <Form.Field required error={!!this.state.body.error}>
+        <Input
+          name="body"
+          placeholder="Body"
+          value={this.state.body.value}
+          onChange={this.handleInputChange}
+        />
+        {this.state.body.error}
+      </Form.Field>
+    );
+
     return (
       <Container>
         <LoaderWrapper isLoading={this.props.isLoading} />
         <Form onSubmit={this.handleFormSubmit}>
-          <Form.Field required error={!!this.state.category.error}>
-            <CategoryDropdownComponent
-              name="category"
-              placeholder="Choose thread category"
-              value={this.state.category.value}
-              onChange={this.handleInputChange}
-              getForumCategories={this.props.getForumCategories}
-            />
-            {this.state.category.error}
-          </Form.Field>
-
-          <Form.Field required error={!!this.state.subject.error}>
-            <Input
-              name="subject"
-              placeholder="Subject"
-              value={this.state.subject.value}
-              onChange={this.handleInputChange}
-            />
-            {this.state.subject.error}
-          </Form.Field>
-
-          <Form.Field required error={!!this.state.body.error}>
-            <Input
-              name="body"
-              placeholder="Body"
-              value={this.state.body.value}
-              onChange={this.handleInputChange}
-            />
-            {this.state.body.error}
-          </Form.Field>
-
+          {CategoryDropdown}
+          {SubjectComponent}
+          {BodyComponent}
           <Button type="submit" disabled={disabled}>
             Post
           </Button>
@@ -84,39 +94,25 @@ ThreadComposer.propTypes = {
   isLoading: PropTypes.bool,
   onPostThread: PropTypes.func,
   categories: PropTypes.array,
-  getForumCategories: PropTypes.func,
 };
 
 export default ThreadComposer;
 
-const threadPost = (subject, body, category) =>
-  axios({
-    method: 'post',
-    url: 'https://thread-204819.appspot.com/postThread',
-    data: {
-      subject,
-      body,
-      category,
-      date: new Date()
-        .toISOString()
-        .substring(0, 19)
-        .replace('T', ' '),
-    },
-    withCredentials: true,
-  });
-
 const validate = (name, value) => {
-  if (name === 'category') {
-    if (!value) return 'Category is required';
-    return '';
-  }
-  if (name === 'subject') {
-    if (value.length < 3) return 'Subject must be > 3 characters';
-    return '';
-  }
-  if (name === 'body') {
-    if (value.length < 10) return 'body must be > 10 characters';
-    return '';
-  }
+  if (name === 'category') return validateCategory(value);
+  if (name === 'subject') return validateSubject(value);
+  if (name === 'body') return validateBody(value);
+};
+
+const validateCategory = value => {
+  if (!value) return 'category is required';
+  return '';
+};
+const validateSubject = value => {
+  if (value.length < 3) return 'Subject must be > 3 characters';
+  return '';
+};
+const validateBody = value => {
+  if (value.length < 10) return 'body must be > 10 characters';
   return '';
 };
